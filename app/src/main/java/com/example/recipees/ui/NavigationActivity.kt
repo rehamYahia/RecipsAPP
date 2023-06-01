e98fefea57a701da6b49cf655135aa500cc84c94
Reham Yahia <asmaa115yahia@gmail.com>
2023-06-01 16:44:18 +0200

    add adapter

--- app/src/main/java/com/example/recipees/ui/NavigationActivity.kt
+++ app/src/main/java/com/example/recipees/ui/NavigationActivity.kt
@@ -1,7 +1,8 @@
-package com.example.recipees
+package com.example.recipees.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.recipees.R
 
 class NavigationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
