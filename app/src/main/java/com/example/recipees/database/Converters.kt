95beb5327a89699ba08bb3e692d174a21e282d9a
Reham Yahia <asmaa115yahia@gmail.com>
2023-06-15 10:38:06 +0200

    editTypeConverter

--- app/src/main/java/com/example/recipees/database/Converters.kt
+++ app/src/main/java/com/example/recipees/database/Converters.kt
@@ -5,13 +5,26 @@ import com.example.recipees.model.Category
 import com.google.gson.Gson
 
 class Converters {
+//    @TypeConverter
+//    public fun fromStringToCategory(stringMeal : String):Category{
+//        return Gson().fromJson(stringMeal , Category::class.java)
+//    }
+//
+//    @TypeConverter
+//    public fun fromCategoryToString(category: Category):String{
+//        return Gson().toJson(category)
+//    }
+
     @TypeConverter
-    public fun fromStringToCategory(stringMeal : String):Category{
-        return Gson().fromJson(stringMeal , Category::class.java)
+    public fun fromStringToList(string :String):ArrayList<Category>{
+       val listType:Type = TypeToken<ArrayList<Category>>{}.getType()
+       return Gson().fromJson(string ,listType)
     }
 
     @TypeConverter
-    public fun fromCategoryToString(category: Category):String{
-        return Gson().toJson(category)
+    public fun fromListToString(list:ArrayList<Category>) : String{
+        val gson :Gson = Gson()
+        val json:String = gson.toJson(list)
+        return json
     }
 }
